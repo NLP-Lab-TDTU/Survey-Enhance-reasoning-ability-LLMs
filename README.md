ad193396d940a8e33a945fd3b911fc438ec3f2e4
Hieu Nguyen <hieunguyen1053@outlook.com>
2024-04-08 11:12:13 +0700

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# References
+
+- [Chain-of-Thought Prompting Elicits Reasoning in Large Language Models](papers/wei2023chainofthought/2201.11903.pdf)
+- [sDPO: Don't Use Your Data All at Once](papers/kim2024sdpo/2403.19270.pdf)
+- [Advancing LLM Reasoning Generalists with Preference Trees](papers/yuan2024advancing/2404.02078.pdf)
+- [ReFT: Representation Finetuning for Language Models](papers/wu2024reft/2404.03592.pdf)
+- [BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding](papers/devlin2019bert/1810.04805.pdf)
---
+++ generate.py
@@ -0,0 +1,39 @@
+import re
+import os
+import glob
+
+def extract_title(content):
+    pattern = r"(?P<field>title)={(?P<value>.*)}"
+    match = re.search(pattern, content)
+    if match:
+        return match.group('value')
+    else:
+        return None
+
+
+def get_pdf_path(citation_file):
+    parent_dir = os.path.dirname(citation_file)
+    pdf_files = glob.glob(f'{parent_dir}/*.pdf')
+    if len(pdf_files) == 1:
+        return pdf_files[0]
+    else:
+        return None
+
+def main():
+    writer = open('README.md', 'w')
+    writer.write('# References\n\n')
+
+    citation_files = glob.glob('papers/**/citation.txt')
+    for file in citation_files:
+        with open(file, 'r') as f:
+            content = f.read()
+            title = extract_title(content)
+            pdf_path = get_pdf_path(file)
+
+            if title and pdf_path:
+                writer.write(f'- [{title}]({pdf_path})\n')
+
+    writer.close()
+
+if __name__ == '__main__':
+    main()
